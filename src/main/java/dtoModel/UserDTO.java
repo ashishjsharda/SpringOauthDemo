77d767176cb075a4c33e827baa213d87b6dcbe10
Ashish Sharda <ashishjsharda@gmail.com>
2023-03-02 06:09:49 -0600

    Add Getter/Setter and constructors

--- src/main/java/dtoModel/UserDTO.java
+++ src/main/java/dtoModel/UserDTO.java
@@ -1,4 +1,41 @@
 package dtoModel;
 
 public class UserDTO {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+
+    public UserDTO(String name, String password, Long id) {
+        this.name = name;
+        this.password = password;
+        this.id = id;
+    }
+
+    private String name;
+    private String password;
+    private Long id;
+
+
 }
